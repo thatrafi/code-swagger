44c3874fdaa4e9193306b689104925406b487111
that_rafi <muhammad.rafiudin@students.amikom.ac.id>
2020-08-02 19:20:31 +0700

    Initial Commit

---
+++ coder swag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coder swag
+//
+//  Created by rafiudin on 02/08/20.
+//  Copyright © 2020 rafiudin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
